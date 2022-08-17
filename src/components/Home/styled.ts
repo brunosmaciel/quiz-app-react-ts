cef49af5700376d715e8bdd654b10546fd46d9d7
Bruno Maciel <brunosazanamaciel@gmail.com>
2022-08-17 16:42:57 -0300

    Resolvido erro no retorno no gameComponnent

--- src/components/Home/styled.ts
+++ src/components/Home/styled.ts
@@ -9,6 +9,7 @@ export const MainContent = styled.div`
   display: flex;
   flex-flow: column wrap;
   align-items: center;
+  justify-content: center;
 `;
 export const Title = styled.h1`
   font-size: 5rem;
